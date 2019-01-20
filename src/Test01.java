edb7f9575ab53f73497839920a3fedd8e3fb5afc
augtsy <18761617801@163.com>
2019-01-20 17:31:42 +0800

    test

--- src/Test01.java
+++ src/Test01.java
@@ -2,5 +2,16 @@ public class Test01 {
 
     public static void main (String [] agrs){
         System.out.println("tsy test01");
+        System.out.println("tsy test02");
+        System.out.println("tsy test03");
+        System.out.println("tsy test04");
+        System.out.println("tsy test05");
+        System.out.println("tsy test06");
+        System.out.println("tsy test07");
+        System.out.println("tsy test08");
+        System.out.println("tsy test09");
+        System.out.println("tsy test10");
+        System.out.println("tsy test01");
+        System.out.println("tsy test01");
     }
 }
